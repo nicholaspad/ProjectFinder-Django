22d312ce654b8b1b99ac0b7ed018da457a0af26b
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-08-22 16:57:52 -0700

    Add Entry model

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Config
+from .models import Config, Entry
 
 admin.site.register(Config)
+admin.site.register(Entry)
 
 ADMIN_SITE_NAME = "ProjectFinder Admin"
 admin.site.site_header = ADMIN_SITE_NAME
--- core/models.py
+++ core/models.py
@@ -6,3 +6,17 @@ class Config(models.Model):
     due_date = models.DateTimeField(
         help_text="Due date (Eastern Time) for ProjectFinder entry"
     )
+
+
+class Entry(models.Model):
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now=True)
+    skills = models.TextField(
+        default="",
+        help_text="Comma-separated list of frameworks and technologies",
+    )
+    interests = models.TextField(
+        default="", max_length=150, help_text="Statement of general project interests"
+    )
+    project_name = models.CharField(max_length=50, default="")
+    project_description = models.TextField(default="", max_length=150)
