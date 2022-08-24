804f014441f95fa877a18ec976ef4387e617d73f
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-08-24 18:43:42 +0530

    Use backend due date in Config model

--- core/views.py
+++ core/views.py
@@ -1,10 +1,14 @@
 from django.views import generic
 
+from .models import Config
+
 
 class IndexView(generic.TemplateView):
     template_name = "core/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        config = Config.objects.first()
         context["netid"] = self.request.user.uniauth_profile.get_display_id()
+        context["config"] = config
         return context
