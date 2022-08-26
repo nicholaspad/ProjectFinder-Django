08cddd58cc02d1dcc85b98718340a55b6b25ff13
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-08-26 22:07:21 +0530

    Automate generating email list, batch when sending

--- core/cron.py
+++ core/cron.py
@@ -1,5 +1,8 @@
 import os
 
+from django.contrib.auth.models import User
+from django.db.models import Q
+
 from .utils import send_email
 
 OVERDUE_SUBJECT = "COS333 ProjectFinder entry past due"
@@ -12,26 +15,46 @@ REMINDER_MESSAGE = """Hello,
 Just a reminder that your ProjectFinder entry is due tomorrow (visit the website for the exact time). Please remember to fill out the project name and description fields!
 - COS333 Staff"""
 
+BATCH_SIZE = 50
+
 
 def send_overdue_email():
     header = f"Subject: {OVERDUE_SUBJECT}\n\n"
     message = header + OVERDUE_MESSAGE
-    to = ["nicholaspad@princeton.edu", "nicholaspad@gmail.com"]
+    to = []
+    for user in User.objects.filter(~Q(username="admin"), entry__isnull=True):
+        if user.username == "admin":
+            continue
+        if user.email:
+            to.append(user.email)
+            continue
+        netid = user.username.split("-")[-1]
+        to.append(f"{netid}@princeton.edu")
 
-    send_email(
-        to,
-        message,
-        os.getenv("EMAIL_PW", ""),
-    )
+    for i in range(0, len(to), BATCH_SIZE):
+        send_email(
+            to[i : i + BATCH_SIZE],
+            message,
+            os.getenv("EMAIL_PW", ""),
+        )
 
 
 def send_reminder_email():
     header = f"Subject: {REMINDER_SUBJECT}\n\n"
     message = header + REMINDER_MESSAGE
-    to = ["nicholaspad@princeton.edu", "nicholaspad@gmail.com"]
+    to = []
+    for user in User.objects.filter(~Q(username="admin")):
+        if user.username == "admin":
+            continue
+        if user.email:
+            to.append(user.email)
+            continue
+        netid = user.username.split("-")[-1]
+        to.append(f"{netid}@princeton.edu")
 
-    send_email(
-        to,
-        message,
-        os.getenv("EMAIL_PW", ""),
-    )
+    for i in range(0, len(to), BATCH_SIZE):
+        send_email(
+            to[i : i + BATCH_SIZE],
+            message,
+            os.getenv("EMAIL_PW", ""),
+        )
