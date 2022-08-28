919c5b95d589db6c9e20b7289dae420988b4de24
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-08-28 08:33:07 +0530

    Remove unnecessary LOGIN_URL ref

--- core/urls.py
+++ core/urls.py
@@ -1,28 +1,24 @@
 from django.urls import path
 from uniauth.decorators import login_required
 
-from ProjectFinder.settings import LOGIN_URL
-
 from . import views
 
 app_name = "core"
 urlpatterns = [
-    path(
-        "", login_required(views.IndexView.as_view(), login_url=LOGIN_URL), name="index"
-    ),
+    path("", login_required(views.IndexView.as_view()), name="index"),
     path(
         "update-settings",
-        login_required(views.UpdateSettingsView.as_view(), login_url=LOGIN_URL),
+        login_required(views.UpdateSettingsView.as_view()),
         name="update-settings",
     ),
     path(
         "create-or-update-entry",
-        login_required(views.CreateOrUpdateEntryView.as_view(), login_url=LOGIN_URL),
+        login_required(views.CreateOrUpdateEntryView.as_view()),
         name="create-or-update-entry",
     ),
     path(
         "delete-entry",
-        login_required(views.DeleteEntryView.as_view(), login_url=LOGIN_URL),
+        login_required(views.DeleteEntryView.as_view()),
         name="delete-entry",
     ),
 ]
