066dc2bc57cf2b8e99af4fe6e990dd2273e91ec3
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-08-28 08:07:19 +0530

    Change STATIC_URL

--- ProjectFinder/settings/base.py
+++ ProjectFinder/settings/base.py
@@ -119,5 +119,5 @@ EMAIL_PW = getenv("EMAIL_PW", "")
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_URL = "static/"
+STATIC_URL = "staticfiles/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
