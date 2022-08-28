327fd5f162de488edbf38e22a2052acf647c69d4
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-08-28 08:11:16 +0530

    Install whitenoise for static file serve in prod

--- ProjectFinder/settings/base.py
+++ ProjectFinder/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -121,3 +122,4 @@ EMAIL_PW = getenv("EMAIL_PW", "")
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = "staticfiles/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
