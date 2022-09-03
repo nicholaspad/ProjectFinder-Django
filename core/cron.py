2e0ba1af98b7ad5699970e8d76d9178bf43d35a2
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-09-03 16:36:18 +0800

    Move email texts into emails.py

--- core/cron.py
+++ core/cron.py
@@ -5,18 +5,9 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 from ProjectFinder.settings import EMAIL_PW
 
+from .emails import OVERDUE_MESSAGE, OVERDUE_SUBJECT, REMINDER_MESSAGE, REMINDER_SUBJECT
 from .models import Config
 from .utils import log_email, send_email
-
-OVERDUE_SUBJECT = "COS333 ProjectFinder entry past due"
-OVERDUE_MESSAGE = """Hello,
-Your project grade has been penalized because you did not create an entry in ProjectFinder before the due date. Please contact course staff if you believe this is incorrect.
-- COS333 Staff"""
-
-REMINDER_SUBJECT = "COS333 ProjectFinder entry reminder"
-REMINDER_MESSAGE = """Hello,
-Just a reminder that your ProjectFinder entry is due tomorrow (visit the website for the exact time). Please remember to fill out the project name and description fields!
-- COS333 Staff"""
 
 BATCH_SIZE = 50
 
---
+++ core/emails.py
@@ -0,0 +1,9 @@
+OVERDUE_SUBJECT = "COS333 ProjectFinder entry past due"
+OVERDUE_MESSAGE = """Hello,
+Your project grade has been penalized because you did not create an entry in ProjectFinder before the due date. Please contact course staff if you believe this is incorrect.
+- COS333 Staff"""
+
+REMINDER_SUBJECT = "COS333 ProjectFinder entry reminder"
+REMINDER_MESSAGE = """Hello,
+Just a reminder that your ProjectFinder entry is due tomorrow (visit the website for the exact time). Please remember to fill out the project name and description fields!
+- COS333 Staff"""
